6be2f7e6e08e0238453a49625c72f793b7dd6d80
courtneyc148 <courtneyc148@gmail.com>
2020-11-18 11:17:06 -0500

    update

--- src/components/FactThree.js
+++ src/components/FactThree.js
@@ -5,7 +5,7 @@ const FactThree = () => {
         <div className="factThree container">
             <div className="factThreeContainer">
                 <p>
-                Among the solo travelers in the G Adventure community, 42% are from the U.  S. The British make up 40% of the site’s solo traveling community.    Australians and Canadians follow closely at 37% and 35% of the solo    traveling population.
+                Among the solo travelers in the G Adventure community, 42% are from the U.S. The British make up 40% of the site’s solo traveling community. Australians and Canadians follow closely at 37% and 35% of the solo traveling population.
                 </p>
             </div>
         </div>
